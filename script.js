132ad9b8ea6f5ee69406d7801a831d28510d114e
Igwedinma Ifechukwu Divine <64442300+I-Divine@users.noreply.github.com>
2024-08-03 01:23:53 +0100

    Initialized files
    -Added assets
    -Styling
    -Navbar
    -Hero Section

---
+++ script.js
@@ -0,0 +1,8 @@
+const navVisibilityOff = () => {
+  const mobileNav = document.getElementById("mobile-nav");
+  mobileNav.classList.add("invisible");
+};
+const navVisibilityOn = () => {
+  const mobileNav = document.getElementById("mobile-nav");
+  mobileNav.classList.remove("invisible");
+};
