ccd299ba763e37cbef32d4f3a43fe11ef89ae15a
Igwedinma Ifechukwu Divine <64442300+I-Divine@users.noreply.github.com>
2024-08-03 03:04:24 +0100

    Added dark mode

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ const navVisibilityOn = () => {
   const mobileNav = document.getElementById("mobile-nav");
   mobileNav.classList.remove("invisible");
 };
+
+let body;
+window.onload = function get_body() {
+  let mediaQueryObj = window.matchMedia("(prefers-color-scheme: dark)");
+  let isDarkMode = mediaQueryObj.matches;
+  body = document.getElementsByTagName("body")[0];
+  if (isDarkMode) {
+    body.classList.add("dark");
+  }
+};
+
+const turnOnDarkMode = () => {
+  body.classList.add("dark");
+};
+const turnOffDarkMode = () => {
+  body.classList.remove("dark");
+};
