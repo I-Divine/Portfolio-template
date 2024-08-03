d0744b816bf56b14adf51a1b266f8a261595b082
Igwedinma Ifechukwu Divine <64442300+I-Divine@users.noreply.github.com>
2024-08-03 14:52:55 +0100

    Update

--- script.js
+++ script.js
@@ -23,3 +23,86 @@ const turnOnDarkMode = () => {
 const turnOffDarkMode = () => {
   body.classList.remove("dark");
 };
+
+const container = document.getElementById("container");
+const slider = document.getElementById("slider");
+const slides = document.getElementsByClassName("slide").length;
+const buttons = document.getElementsByClassName("btn");
+
+console.log(container);
+
+var currentPosition = 0;
+var currentMargin = 0;
+var slidesPerPage = 0;
+var slidesCount = slides - slidesPerPage;
+var containerWidth = container.offsetWidth;
+var prevKeyActive = false;
+var nextKeyActive = true;
+
+window.addEventListener("resize", checkWidth);
+
+function checkWidth() {
+  containerWidth = container.offsetWidth;
+  setParams(containerWidth);
+}
+
+function setParams(w) {
+  if (w < 551) {
+    slidesPerPage = 1;
+  } else {
+    if (w < 901) {
+      slidesPerPage = 2;
+    } else {
+      if (w < 1101) {
+        slidesPerPage = 3;
+      } else {
+        slidesPerPage = 4;
+      }
+    }
+  }
+  slidesCount = slides - slidesPerPage;
+  if (currentPosition > slidesCount) {
+    currentPosition -= slidesPerPage;
+  }
+  currentMargin = -currentPosition * (100 / slidesPerPage);
+  slider.style.marginLeft = currentMargin + "%";
+  if (currentPosition > 0) {
+    buttons[0].classList.remove("inactive");
+  }
+  if (currentPosition < slidesCount) {
+    buttons[1].classList.remove("inactive");
+  }
+  if (currentPosition >= slidesCount) {
+    buttons[1].classList.add("inactive");
+  }
+}
+
+setParams();
+
+function slideRight() {
+  if (currentPosition != 0) {
+    slider.style.marginLeft = currentMargin + 100 / slidesPerPage + "%";
+    currentMargin += 100 / slidesPerPage;
+    currentPosition--;
+  }
+  if (currentPosition === 0) {
+    buttons[0].classList.add("inactive");
+  }
+  if (currentPosition < slidesCount) {
+    buttons[1].classList.remove("inactive");
+  }
+}
+
+function slideLeft() {
+  if (currentPosition != slidesCount) {
+    slider.style.marginLeft = currentMargin - 100 / slidesPerPage + "%";
+    currentMargin -= 100 / slidesPerPage;
+    currentPosition++;
+  }
+  if (currentPosition == slidesCount) {
+    buttons[1].classList.add("inactive");
+  }
+  if (currentPosition > 0) {
+    buttons[0].classList.remove("inactive");
+  }
+}
